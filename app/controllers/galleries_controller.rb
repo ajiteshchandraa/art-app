f9c8da6f59882ee84b86c5a5cdd2d854cb270e4b
ajitesh chandra <chandra.ajitesh1@yahoo.com>
2023-08-24 13:44:06 +0100

    Added galleries controller and related views

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,47 @@
+class GalleriesController < ApplicationController
+
+  def index
+    @galleries = Gallery.all
+  end
+
+  def show
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    @gallery = Gallery.create(gallery_params)
+
+    redirect_to @gallery
+  end
+
+  def edit
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def update
+    @gallery = Gallery.find(params[:id])
+
+    if @gallery.update(gallery_params)
+      redirect_to @gallery
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @gallery = Gallery.find(params[:id])
+    @gallery.destroy
+
+    redirect_to galleries_path
+  end
+
+  private
+
+  def gallery_params
+    params.require(:gallery).permit(:name, :location)
+  end
+end
